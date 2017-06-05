ec0ec8a3db0bd5d6c8e6ad9fed3eab13e27e9936
guidoux <pedroguidoux@gmail.com>
2017-06-05 12:19:06 -0300

    Triangle exercises

--- src/main/java/drawier/Drawier.java
+++ src/main/java/drawier/Drawier.java
@@ -1,9 +1,21 @@
 package drawier;
 
 public class Drawier {
+
     public static void main(String[] args) {
-        System.out.println('*');
+        Drawier drawier = new Drawier();
+        System.out.println("Triangle Exercises:");
+        System.out.println("1- The easiest exercise ever:");
+        System.out.println(drawier.asterisk());
+        System.out.println("2- Draw horizontal line:");
+        System.out.println(drawier.drawLine(3));
+        System.out.println("3- Draw vertical line:");
+        drawier.printAnAsteriskPerLine(3);
+        System.out.println("4- Draw a right triangle:");
+        drawier.printRightTriangle(3);
     }
+
+    public Drawier() {}
 
     public char asterisk() {
         return '*';
@@ -15,5 +27,17 @@ public class Drawier {
             line += '*';
         }
         return line;
+    }
+
+    public void printAnAsteriskPerLine(int numberOfLines) {
+        for(int i=0; i<numberOfLines; i++) {
+            System.out.println('*');
+        }
+    }
+
+    public void printRightTriangle(int numberOfLines) {
+        for(int i=1; i<=numberOfLines; i++) {
+            System.out.println(drawLine(i));
+        }
     }
 }
